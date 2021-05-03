57c4b7f17be71eabc54ad8a4761a707ac06d1785
hunar <35507228+hunar-art@users.noreply.github.com>
2021-05-03 13:21:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-include_once('index.html')
-include_once('about.html')
-include_once('contact.html')
-include_once('product.html')
-include_once('product_detail3.html')
-include_once('blog.html')
+include_once('index.html');
+include_once('about.html');
+include_once('contact.html');
+include_once('product.html');
+include_once('product_detail3.html');
+include_once('blog.html');
 
 
 ?>
